c5b306b6b4e3686b4921088315e2b29354dc2317
DieGon7771 <226565201+DieGon7771@users.noreply.github.com>
2026-07-10 14:19:34 +0200

    Update VotingApi.kt

--- app/src/main/java/com/lagradost/cloudstream3/plugins/VotingApi.kt
+++ app/src/main/java/com/lagradost/cloudstream3/plugins/VotingApi.kt
@@ -1,5 +1,6 @@
 package com.lagradost.cloudstream3.plugins
 
+
 import android.util.Log
 import android.widget.Toast
 import com.lagradost.cloudstream3.CloudStreamApp.Companion.context
@@ -12,10 +13,11 @@ import com.lagradost.cloudstream3.utils.Coroutines.main
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
-object VotingApi {
 
+object VotingApi {
     private const val LOGKEY = "VotingApi"
-    private const val API_DOMAIN = "https://api.countify.xyz"
+    private const val API_DOMAIN = "https://voting-api.diegon7771.workers.dev"
+
 
     private fun transformUrl(url: String): String =
         MessageDigest
@@ -23,40 +25,49 @@ object VotingApi {
             .digest("${url}#funny-salt".toByteArray())
             .fold("") { str, it -> str + "%02x".format(it) }
 
+
     suspend fun SitePlugin.getVotes(): Int = getVotes(url)
     fun SitePlugin.hasVoted(): Boolean = hasVoted(url)
     suspend fun SitePlugin.vote(): Int = vote(url)
     fun SitePlugin.canVote(): Boolean = canVote(this.url)
 
+
     private val votesCache = mutableMapOf<String, Int>()
+
 
     private suspend fun readVote(pluginUrl: String): Int {
         val id = transformUrl(pluginUrl)
-        val url = "$API_DOMAIN/get-total/$id"
+        val url = "$API_DOMAIN/?id=$id"
         Log.d(LOGKEY, "Requesting GET: $url")
-        return app.get(url).parsedSafe<CountifyResult>()?.count ?: 0
+        return app.get(url).parsedSafe<WorkerResult>()?.count ?: 0
     }
+
 
     private suspend fun writeVote(pluginUrl: String): Boolean {
         val id = transformUrl(pluginUrl)
-        val url = "$API_DOMAIN/increment/$id"
+        val url = "$API_DOMAIN/?id=$id"
         Log.d(LOGKEY, "Requesting POST: $url")
         return app.post(url, emptyMap<String, String>())
-            .parsedSafe<CountifyResult>()?.count != null
+            .parsedSafe<WorkerResult>()?.count != null
     }
+
 
     suspend fun getVotes(pluginUrl: String): Int =
         votesCache[pluginUrl] ?: readVote(pluginUrl).also {
             votesCache[pluginUrl] = it
         }
 
-    fun hasVoted(pluginUrl: String) =
-        getKey("cs3-votes/${transformUrl(pluginUrl)}") ?: false
+
+    fun hasVoted(pluginUrl: String): Boolean =
+        getKey<Boolean>("cs3-votes/${transformUrl(pluginUrl)}") ?: false
+
 
     fun canVote(pluginUrl: String): Boolean =
         PluginManager.urlPlugins.contains(pluginUrl)
 
+
     private val voteLock = Mutex()
+
 
     suspend fun vote(pluginUrl: String): Int {
         voteLock.withLock {
@@ -71,6 +82,7 @@ object VotingApi {
                 return getVotes(pluginUrl)
             }
 
+
             if (hasVoted(pluginUrl)) {
                 main {
                     Toast.makeText(
@@ -82,17 +94,21 @@ object VotingApi {
                 return getVotes(pluginUrl)
             }
 
+
             if (writeVote(pluginUrl)) {
                 setKey("cs3-votes/${transformUrl(pluginUrl)}", true)
                 votesCache[pluginUrl] = votesCache[pluginUrl]?.plus(1) ?: 1
             }
 
+
             return getVotes(pluginUrl)
         }
     }
 
-    private data class CountifyResult(
+
+    private data class WorkerResult(
         val id: String? = null,
-        val count: Int? = null
+        val count: Int? = null,
     )
 }
+
